6895d4098d70f8b5b9b10843d5f7ba847feee24c
Bedro <pedromppedromp@gmail.com>
2020-12-24 16:12:36 +0100

    site setting

--- main/models.py
+++ main/models.py
@@ -1,11 +1,22 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 from django.db import models
 # Create your models here.
 
 
 class Main(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=30)
+    about = models.TextField()
+    fb = models.CharField(default="-", max_length=150)
+    tw = models.CharField(default="-", max_length=150)
+    yt = models.CharField(default="-", max_length=150)
+    tell = models.CharField(default="-", max_length=30)
+    link = models.CharField(default="-", max_length=30)
+
+    set_name = models.CharField(default="-", max_length=30)
 
 
+
+    # def __str__(self):
+    #     return self.name
     def __str__(self):
-        return self.name
+        return self.set_name + " | " + str(self.pk)
--- main/views.py
+++ main/views.py
@@ -4,11 +4,15 @@ from .models import Main
 
 
 def home(request):
+    # sitename = "My Site | Home"
+    # site = Main.objects.all()
+    # site = Main.objects.filter(pk=1)
+    site = Main.objects.get(pk=1)
 
-
-    return render(request, 'front/home.html')
+    return render(request, 'front/home.html', {'site':site}) #{'sitename':sitename}
 
 def about(request):
+    # sitename = "My Site | About"
+    site = Main.objects.get(pk=1)
 
-
-    return render(request, 'front/about.html')
+    return render(request, 'front/about.html', {'site':site}) #{'sitename':sitename}
